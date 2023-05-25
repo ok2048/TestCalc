18276a4a732739ff9ee5ca76afe9e3a5a1bd077a
ok2048 <ok2048@yandex.ru>
2023-05-25 22:34:13 +0300

    Project uploaded

---
+++ tests/test_calc.py
@@ -0,0 +1,27 @@
+import pytest
+
+from app.calc import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_adding(self):
+        assert self.calc.adding(self, 1, 2) == 3
+
+    def test_multiply(self):
+        assert self.calc.multiply(self, 2, 3) == 6
+
+    def test_subtraction(self):
+        assert self.calc.subtraction(self, 3, 2) == 1
+
+    def test_division(self):
+        assert self.calc.division(self, 6, 2) == 3
+
+    def test_zero_division(self):
+        with pytest.raises(ZeroDivisionError):
+            self.calc.division(self,1, 0)
+
+    def teardown(self):
+        pass
+
